72ff08f9a85415c42a41bdaf62a62515377fdce5
Rohan Verma <rv725555@gmail.com>
2023-02-22 22:47:17 +0530

    test: #6 Trying out 'default imports' of lodash, resulting in extreme bundle size increase.

--- public/ferrari.js
+++ public/ferrari.js
@@ -1,1 +1,19 @@
+import _ from 'lodash'
+
+// Nested object
+const obj = {
+    one: {
+        two: {
+            three: {
+                four: "Bruh",
+            },
+        },
+    },
+}
+
+const getBruh = _.get(obj, "one.three.four", {});
+
+// console.log(getBruh)
+
 export const ferrari = "Is timeless in red and but I would like it in black just to rebel against purists"
+
